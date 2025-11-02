45e32fe460d5a8e80fef9c821d979a15313d11c8
01Ander <arinconayala@hotmail.com>
2025-11-01 21:43:53 -0400

    firts part exercise data types funcions and compresions

---
+++ exercise_streaming_sessions_analytics.py
@@ -0,0 +1,110 @@
+# Day 1
+sessions_day1 = [
+    ("u01", "Aurora", 42, "2025-10-28", "mobile"),
+    ("u02", "Nebula", 15, "2025-10-28", "tv"),
+    ("u03", "Aurora", 57, "2025-10-28", "desktop"),
+    ("u02", "Orion", 33, "2025-10-28", "mobile"),
+    ("u04", "Aurora", 12, "2025-10-28", "tablet"),
+    ("u05", "Nebula", 48, "2025-10-28", "tv"),
+    ("u03", "Orion", 21, "2025-10-28", "desktop"),
+    ("u06", "Quasar", 66, "2025-10-28", "mobile"),
+    ("u02", "Aurora", 9,  "2025-10-28", "tv"),
+    ("u07", "Quasar", 18, "2025-10-28", "tv"),
+]
+
+# Day 2
+sessions_day2 = [
+    ("u02", "Aurora", 25, "2025-10-29", "mobile"),
+    ("u08", "Nebula", 51, "2025-10-29", "desktop"),
+    ("u01", "Quasar", 39, "2025-10-29", "tv"),
+    ("u09", "Orion", 10, "2025-10-29", "mobile"),
+    ("u03", "Aurora", 44, "2025-10-29", "desktop"),
+    ("u10", "Quasar", 73, "2025-10-29", "tv"),
+    ("u05", "Nebula", 11, "2025-10-29", "mobile"),
+    ("u06", "Quasar", 30, "2025-10-29", "desktop"),
+    ("u11", "Aurora", 22, "2025-10-29", "tablet"),
+    ("u02", "Orion", 27, "2025-10-29", "tv"),
+]
+
+# --------------item 1
+
+
+def get_data(sessions, value):
+    data_day = set()
+    for data in sessions:
+        data_day.add(data[value])
+    return data_day
+
+
+users_day1 = get_data(sessions_day1, 0)
+users_day2 = get_data(sessions_day2, 0)
+titles_day1 = get_data(sessions_day1, 1)
+titles_day2 = get_data(sessions_day2, 1)
+
+print(titles_day1, titles_day2)
+
+users_same_days = users_day1 & users_day2
+print(f'Users on both days: {users_same_days}')
+new_users_day2 = users_day2 - users_day1
+print(f'New users on Day 2: {new_users_day2}')
+all_titles = titles_day1 | titles_day2
+print(f'Total catalog of titles viewed on both days: {all_titles}')
+titles_view_day1 = titles_day1 - titles_day2
+print(f'Titles viewed only on Day 1: {titles_view_day1}')
+
+# ----------item2
+# formato por acumulación, mas eficiente
+
+title_minutes_day1 = {}
+
+for session in sessions_day1:
+    title = session[1]
+    minutes = session[2]
+    title_minutes_day1[title] = title_minutes_day1.get(title, 0) + minutes
+
+print(title_minutes_day1)
+
+# formato por compresion, mas recursos
+title_minutes_day2 = {title: sum(session[2] for session in sessions_day1 if session[1] == title)
+                      for title in set(session[1] for session in sessions_day1)}
+print(title_minutes_day2)
+
+
+# --------------item3
+# user_stats_day1 = {}
+
+# for session in sessions_day1:
+#     user_id = session[0]
+#     minutes = session[2]
+#     # user_stats_day1[user_id] = user_stats_day1.get(user_id, 0) + minutes
+#     if user_id not in user_stats_day1:
+#         user_stats_day1[user_id] = {
+#             'total_minutes': minutes, "session_count": 1}
+#     else:
+#         user_stats_day1[user_id]['total_minutes'] += minutes
+#         user_stats_day1[user_id]['session_count'] += 1
+
+# print(user_stats_day1)
+
+
+def get_user_stats(sessions):
+    user_stats = {}
+    for session in sessions:
+        user_id = session[0]
+        minutes = session[2]
+        if user_id not in user_stats:
+            user_stats[user_id] = {
+                'total_minutes': minutes, "session_count": 1}
+        else:
+            user_stats[user_id]['total_minutes'] += minutes
+            user_stats[user_id]['session_count'] += 1
+    return user_stats
+
+
+users_stats_day1 = get_user_stats(sessions_day1)
+users_stats_day2 = get_user_stats(sessions_day2)
+
+for user in users_stats_day1:
+    top_user = max(users_stats_day1.items(),
+                   key=lambda item: item[1]['total_minutes'])
+print(top_user)
--- sets.py
+++ sets.py
@@ -9,3 +9,16 @@ print(set_a & set_b)
 print(set_a - set_b)
 print(set_a ^ set_b)
 print(len(set_a | set_b))
+
+
+# # Unión (valores de ambos)
+# print(a | b)   # {1, 2, 3, 4, 5, 6}
+
+# # Intersección (valores en común)
+# print(a & b)   # {3, 4}
+
+# # Diferencia (valores solo en 'a')
+# print(a - b)   # {1, 2}
+
+# # Diferencia simétrica (valores que están en uno u otro, pero no en ambos)
+# print(a ^ b)   # {1, 2, 5, 6}
