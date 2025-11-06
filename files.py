26a362fd33861a17a0d90a5a48db2c7924aa25ea
01Ander <arinconayala@hotmail.com>
2025-11-06 17:32:12 -0500

    pathlib

--- files.py
+++ files.py
@@ -31,3 +31,37 @@ with open('files/usuarios.csv', 'r', encoding='utf-8') as f:
     reader = csv.DictReader(f)
     for fila in reader:
         print(fila['name'], '→', fila['age'])
+
+
+# import json
+# write
+# usuario = {
+#     "nombre": "Andersson",
+#     "edad": 30,
+#     "skills": ["Python", "Data", "QA"]
+# }
+
+# with open("usuario.json", "w", encoding="utf-8") as f:
+#     json.dump(usuario, f, indent=4, ensure_ascii=False)
+
+# read
+# with open("usuario.json", "r", encoding="utf-8") as f:
+#     data = json.load(f)
+
+# print(data["skills"])
+
+
+# from pathlib import Path
+
+# # Carpeta
+# ruta_carpeta = Path("data")
+# ruta_carpeta.mkdir(exist_ok=True)  # la crea si no existe
+
+# # Archivo dentro de la carpeta
+# ruta_archivo = ruta_carpeta / "ejemplo.txt"
+
+# # Escribir
+# ruta_archivo.write_text("Hola desde pathlib", encoding="utf-8")
+
+# # Leer
+# print(ruta_archivo.read_text(encoding="utf-8"))
