a5c970be2138bf0ebfa3a339d8317b01016bebae
01Ander <arinconayala@hotmail.com>
2025-11-06 17:30:24 -0500

    all files

--- files.py
+++ files.py
@@ -8,12 +8,26 @@
 #     f.write('Linea 3\n')
 
 # read
-with open("notas.txt", "r", encoding="utf-8") as f:
-    contenido = f.read()
+# with open("notas.txt", "r", encoding="utf-8") as f:
+#     contenido = f.read()
 
-print(contenido)
+# print(contenido)
+
+# read all
+# with open("notas.txt", "r", encoding="utf-8") as a:
+#     for linea in a:
+#         print(linea.strip())  # strip() quita \n y espacios laterales
 
 
-with open("notas.txt", "r", encoding="utf-8") as f:
-    for linea in f:
-        print(linea.strip())  # strip() quita \n y espacios laterales
+import csv
+
+# with open('files/usuarios.csv', 'w', newline='', encoding='utf-8') as f:
+#     writer = csv.writer(f)
+#     writer.writerow(['name', 'age'])
+#     writer.writerow(['andersson', 31])
+#     writer.writerow(['Ana', 40])
+
+with open('files/usuarios.csv', 'r', encoding='utf-8') as f:
+    reader = csv.DictReader(f)
+    for fila in reader:
+        print(fila['name'], '→', fila['age'])
