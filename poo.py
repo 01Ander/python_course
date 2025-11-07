c562812564de4686b7ee4a1ffb228e91b69908c3
01Ander <arinconayala@hotmail.com>
2025-11-07 17:33:05 -0500

    dunder methods

--- poo.py
+++ poo.py
@@ -33,3 +33,24 @@ book2 = Book('Harry Potter', 'J.K. Rolling', '1997')
 
 book1.get_info()
 book2.get_info()
+
+
+class Rectangle:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def area(self):
+        return self.width * self.height
+
+    def perimetre(self):
+        return (2 * self.width) + (2 * self.height)
+
+    def __str__(self):
+        return f"Rectangle dimension {self.width}x{self.height}"
+
+
+rectangle = Rectangle(5, 8)
+print(rectangle.area())
+print(rectangle.perimetre())
+print(rectangle)
